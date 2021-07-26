837f977ad68c1a2d4eecf4b8c2d50c5a05ef99d9
mikelo <michele.orlandi@gmail.com>
2021-07-26 11:07:18 +0200

    added metrics and scheduler (?)

--- src/main/java/ibm/com/stivaleprimaverile/stivaleprimaverile/StivaleprimaverileApplication.java
+++ src/main/java/ibm/com/stivaleprimaverile/stivaleprimaverile/StivaleprimaverileApplication.java
@@ -2,8 +2,10 @@ package ibm.com.stivaleprimaverile.stivaleprimaverile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StivaleprimaverileApplication {
 
 	public static void main(String[] args) {
