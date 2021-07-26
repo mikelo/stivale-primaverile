13cc75a388893ead50f18da7d429b4d0b8762f62
mikelo <michele.orlandi@gmail.com>
2021-07-26 10:22:44 +0200

    merge done again

---
+++ src/main/java/ibm/com/stivaleprimaverile/stivaleprimaverile/StivaleprimaverileApplication.java
@@ -0,0 +1,13 @@
+package ibm.com.stivaleprimaverile.stivaleprimaverile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StivaleprimaverileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StivaleprimaverileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ibm/com/stivaleprimaverile/stivaleprimaverile/StivaleprimaverileApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.com.stivaleprimaverile.stivaleprimaverile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StivaleprimaverileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
